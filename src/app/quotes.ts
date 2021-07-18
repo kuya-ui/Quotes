5633807e6c1d6a1344c8555551ae8ddb3d27bea2
kuya-ui <kuyamaxmillan@gmail.com>
2021-07-18 18:55:48 +0300

    description

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,6 @@
 export class Quotes {
+    public showDescription: boolean
+    constructor(public id: number, public name: string, public description: string, public completeDate: Date, public like: number, public dislike: number){
+        this.showDescription=false
+    }
 }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,7 +9,7 @@ import { Quotes } from '../quotes';
 export class QuotesComponent implements OnInit {
   quote: Quotes[] =[
     new Quotes(1,'Success is most often achieved by those who dont know that failure is unevitable.' 'Coco Chanel', new Date(2021,2,3)),
-    new Quotes(2,'Have the courage to follow your heart and intution, Ther somehow know what you truly want to become.' 'Steve Jobs', new Date(2021,18,7)),
+    new Quotes(2,'Have the courage to follow your heart and intution, There somehow know what you truly want to become.' 'Steve Jobs', new Date(2021,18,7)),
     new Quotes(3,'Dont feel stupid if you dont like what everyone else pretends to love' 'Emma Watson', new Date(2021,6,29))
   ]
   constructor() { }
