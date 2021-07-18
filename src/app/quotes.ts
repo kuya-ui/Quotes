2569301afb3f4c3b08aa2248ab79500f8302c260
kuya-ui <kuyamaxmillan@gmail.com>
2021-07-18 16:28:26 +0300

    changes

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,5 @@
 export class Quotes {
+    constructor( public id: number, public name: string, public description: string, public completeDate: Date, public like: number, public dislike: number){
+        // this.showDescription = false;
+    }
 }
