f9500d7723188fec8795e8a4429ec7cee0911a71
kuya-ui <kuyamaxmillan@gmail.com>
2021-07-18 21:47:56 +0300

    changes

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -8,9 +8,9 @@ import { Quotes } from '../quotes';
 })
 export class QuotesComponent implements OnInit {
   quotes:Quotes[] =[
-    new Quotes(1,'Success is most often achieved by those who dont know that failure is unevitable.' 'Coco Chanel', new Date(2021,2,3)),
-    new Quotes(2,'Have the courage to follow your heart and intution, There somehow know what you truly want to become.' 'Steve Jobs', new Date(2021,18,7)),
-    new Quotes(3,'Dont feel stupid if you dont like what everyone else pretends to love' 'Emma Watson', new Date(2021,6,29))
+    new Quotes(1,"Success is most often achieved by those who don't know that failure is inevitable",'Coco Chanel', new Date(2021,2,3),1,1),
+    new Quotes(2,"Have the courage to follow your heart and intuition, There somehow know what you truly want to become", 'Steve Jobs', new Date(2021,18,7),1,1),
+    new Quotes(2,"don't feel stupid if you don't like what everyone else pretends to love", 'Emma Watson', new Date(2021,3,5),1,1)
   ]
   constructor() { }
 
